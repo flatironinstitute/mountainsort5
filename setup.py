a8165b52d4cbfdf6a5f0b98795c2bab9786f144c
Jeremy Magland <jmagland@flatironinstitute.org>
2023-03-28 09:29:19 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 from setuptools import setup, find_packages
 
+# read the contents of README.md
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+__version__ = '0.1.1'
+
 setup(
+    name='mountainsort5',
+    version=__version__,
+    author="Jeremy Magland",
+    author_email="jmagland@flatironinstitute.org",
+    url="https://github.com/magland/mountainsort5",
+    description="MountainSort 5 spike sorting algorithm",
+    long_description=long_description,
     packages=find_packages(),
     install_requires=[
         'spikeinterface>=0.97.1',

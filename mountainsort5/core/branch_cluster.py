fca12bf7d4385f226684815580716564fb851565
Jeremy Magland <jmagland@flatironinstitute.org>
2023-05-08 15:49:18 -0400

    don't do experimental merge for now

--- mountainsort5/core/branch_cluster.py
+++ mountainsort5/core/branch_cluster.py
@@ -35,42 +35,43 @@ def branch_cluster(
         for k2 in range(1, K2 + 1):
             last_ret_k += 1
             ret[inds0[labels0 == k2]] = last_ret_k
-    # test merging of clusters
-    K_total = np.max(ret)
-    to_merge = []
-    for k1 in range(1, K_total + 1):
-        inds1 = np.nonzero(ret == k1)[0]
-        for k2 in range(k1 + 1, K_total + 1):
-            inds2 = np.nonzero(ret == k2)[0]
-            if len(inds1) == 0 or len(inds2) == 0:
-                continue
-            X1 = X[inds1]
-            X2 = X[inds2]
-            if _should_merge(X1, X2):
-                to_merge.append((k1, k2))
+    return ret
+    # # test merging of clusters
+    # K_total = np.max(ret)
+    # to_merge = []
+    # for k1 in range(1, K_total + 1):
+    #     inds1 = np.nonzero(ret == k1)[0]
+    #     for k2 in range(k1 + 1, K_total + 1):
+    #         inds2 = np.nonzero(ret == k2)[0]
+    #         if len(inds1) == 0 or len(inds2) == 0:
+    #             continue
+    #         X1 = X[inds1]
+    #         X2 = X[inds2]
+    #         if _should_merge(X1, X2):
+    #             to_merge.append((k1, k2))
 
-    something_changed = True
-    while something_changed:
-        something_changed = False
-        for merge_item in to_merge:
-            k1, k2 = merge_item
-            inds1 = np.nonzero(ret == k1)[0]
-            inds2 = np.nonzero(ret == k2)[0]
-            if len(inds1) == 0 or len(inds2) == 0:
-                continue
-            print(f'Merging {k1} and {k2}')
-            ret[inds2] = k1
-            something_changed = True
-    # consolidate labels
-    K_total = np.max(ret)
-    ret2 = np.zeros((L,), dtype=np.int32)
-    k2 = 1
-    for k in range(1, K_total + 1):
-        inds = np.nonzero(ret == k)[0]
-        if len(inds) > 0:
-            ret2[inds] = k2
-            k2 += 1
-    return ret2
+    # something_changed = True
+    # while something_changed:
+    #     something_changed = False
+    #     for merge_item in to_merge:
+    #         k1, k2 = merge_item
+    #         inds1 = np.nonzero(ret == k1)[0]
+    #         inds2 = np.nonzero(ret == k2)[0]
+    #         if len(inds1) == 0 or len(inds2) == 0:
+    #             continue
+    #         print(f'Merging {k1} and {k2}')
+    #         ret[inds2] = k1
+    #         something_changed = True
+    # # consolidate labels
+    # K_total = np.max(ret)
+    # ret2 = np.zeros((L,), dtype=np.int32)
+    # k2 = 1
+    # for k in range(1, K_total + 1):
+    #     inds = np.nonzero(ret == k)[0]
+    #     if len(inds) > 0:
+    #         ret2[inds] = k2
+    #         k2 += 1
+    # return ret2
 
 def _should_merge(X1: npt.NDArray[np.float32], X2: npt.NDArray[np.float32]):
     discriminating_direction = np.mean(X2, axis=0) - np.mean(X1, axis=0)

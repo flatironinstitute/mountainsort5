afaf1037a91da696320b1b05272feb2eb31d55aa
Jeremy Magland <jmagland@flatironinstitute.org>
2023-10-03 10:33:30 -0400

    in remove_duplicate_times, check for empty times

--- mountainsort5/schemes/sorting_scheme1.py
+++ mountainsort5/schemes/sorting_scheme1.py
@@ -150,6 +150,8 @@ def sorting_scheme1(
     return sorting
 
 def remove_duplicate_times(times: npt.NDArray[np.int32], labels: npt.NDArray[np.int32]):
+    if len(times) == 0:
+        return times, labels
     inds = np.where(np.diff(times) > 0)[0]
     inds = np.concatenate([[0], inds + 1])
     times2 = times[inds]

cf6534d5dd17b1b912d7605738902044a430ac25
Jeremy Magland <jmagland@flatironinstitute.org>
2024-03-01 06:47:52 -0500

    linter fix

--- mountainsort5/util/__init__.py
+++ mountainsort5/util/__init__.py
@@ -1,4 +1,3 @@
 from .TemporaryDirectory import TemporaryDirectory # noqa: F401
 from .create_cached_recording import create_cached_recording # noqa: F401
-from .binary_recording_io import save_binary_recording, load_binary_recording
-
+from .binary_recording_io import save_binary_recording, load_binary_recording # noqa: F401

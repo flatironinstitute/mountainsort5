96d83fae303a62b131268c5d8ce709f6f048af11
Jeremy Magland <jmagland@flatironinstitute.org>
2025-04-04 13:49:59 -0400

    incr version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-__version__ = '0.5.7'
+__version__ = '0.5.8'
 
 setup(
     name='mountainsort5',
